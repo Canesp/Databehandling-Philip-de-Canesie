2a46fe520c8ccdca634f0861d8f381ce1c25e670
Canesp <philip.decanesie@gmail.com>
2022-11-08 10:01:16 +0100

    L5 import and load data

---
+++ Code-alongs/L5_socky_dashbord/main.py
@@ -0,0 +1,12 @@
+import dash
+import dash_bootstrap_components as dbc
+import os
+from load_data import StockData
+
+directory_path = os.path.dirname(__file__)
+path = os.path.join(directory_path, "stockdata")
+
+print(path)
+
+stockdata_object = StockData(path)
+print(stockdata_object.stock_dataframe("AAPL"))
