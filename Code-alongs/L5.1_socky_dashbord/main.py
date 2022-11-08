c9b1b8fe2edda57cccfdb9d4636953f661e30692
Canesp <philip.decanesie@gmail.com>
2022-11-08 12:08:46 +0100

    L5.1 dashboard

--- Code-alongs/L5.1_socky_dashbord/main.py
+++ Code-alongs/L5.1_socky_dashbord/main.py
@@ -6,6 +6,7 @@ from dash import html, dcc
 from dash.dependencies import Output, Input
 import plotly_express as px
 from time_filtering import filter_time
+import pandas as pd
 
 directory_path = os.path.dirname(__file__)
 path = os.path.join(directory_path, "stockdata")
@@ -51,32 +52,66 @@ app.layout = html.Main(
             options=stock_options_dropdown,
             value="AAPL",
         ),
+        html.P(id="highest-value"),
+        html.P(id="lowest-value"),
         dcc.RadioItems(id="ohlc-radio", options=ohlc_options, value="close"),
         dcc.Graph(id="stock-graph"),
         dcc.Slider(
             id="time-slider", min=0, max=6, marks=slider_marks, value=2, step=None
         ),
+        # storing intermediate value on clients browser in order to share between several callbacks
+        dcc.Store(id="filtered-df"),
     ]
 )
 
-@app.callback(
-    Output("stock-graph", "figure"),
-    Input("stockpicker-dropdown", "value"),
-    Input("ohlc-radio", "value"),
-    Input("time-slider", "value")
-)
 
-def update_graph(stock, ohlc, time_index):
+@app.callback(
+    Output("filtered-df", "data"),
+    Input("stockpicker-dropdown", "value"),
+    Input("time-slider", "value"),
+)
+def filter_df(stock, time_index):
     dff_daily, dff_intraday = df_dict[stock]
 
     dff = dff_intraday if time_index <= 2 else dff_daily
 
-    days = {i: day for i, day in enumerate([1, 7, 30, 90, 365, 365*5])}
+    days = {i: day for i, day in enumerate([1, 7, 30, 90, 365, 365 * 5])}
 
-    dff = dff if time_index == 6 else filter_time(dff, days= days[time_index])
+    dff = dff if time_index == 6 else filter_time(dff, days=days[time_index])
 
-    return px.line(dff, x= dff.index, y= ohlc, title= symbol_dict[stock])
-    
+    return dff.to_json()
+
+
+@app.callback(
+    Output("highest-value", "children"),
+    Output("lowest-value", "children"),
+    Input("filtered-df", "data"),
+    Input("ohlc-radio", "value"),
+)
+def highest_lowest_value_update(json_df, ohlc):
+    dff = pd.read_json(json_df)
+    highest_value = dff[ohlc].max()
+    lowest_value = dff[ohlc].min()
+    return highest_value, lowest_value
+
+
+@app.callback(
+    Output("stock-graph", "figure"),
+    Input("filtered-df", "data"),
+    Input("stockpicker-dropdown", "value"),
+    Input("ohlc-radio", "value"),
+)
+def update_graph(json_df, stock, ohlc):
+    # dff_daily, dff_intraday = df_dict[stock]
+
+    # dff = dff_intraday if time_index <= 2 else dff_daily
+
+    # days = {i: day for i, day in enumerate([1, 7, 30, 90, 365, 365 * 5])}
+
+    # dff = dff if time_index == 6 else filter_time(dff, days=days[time_index])
+    dff = pd.read_json(json_df)
+    return px.line(dff, x=dff.index, y=ohlc, title=symbol_dict[stock])
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
